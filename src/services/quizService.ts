19884434ef839a231167c13086b7f7e34f112abe
adharshkc <adharshkc2017@gmail.com>
2024-12-16 23:10:20 +0530

    fix: added some changes

--- src/services/quizService.ts
+++ src/services/quizService.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 
 export const quizService = {
     fetchQuiz: async()=>{
-        const quiz = await axios.get(import.meta.env.VITE_quiz_api)
+        const quiz = await axios.get('https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple&encode=base64')
         return quiz
     }
 }
